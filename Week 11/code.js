e9e158110c0861ce65310011d1f708a85a19a35a
0xLLLLH <linlihao159@gmail.com>
2020-12-21 00:21:03 +0800

    Week 11: Bugfix

---
+++ Week 11/code.js
@@ -0,0 +1,49 @@
+function match(selector, element) {
+    const fragment = selector.split(" ").reverse();
+    const matchFragment = (frag, el) => {
+        if (!frag || frag === "*") {
+            return true;
+        }
+        // div
+        if (el.tagName === frag) {
+            return true;
+        }
+        // .class
+        if (el.classList.contains(frag.slice(1))) {
+            return true;
+        }
+        // #id
+        if (el.id === frag.slice(1)) {
+            return true;
+        }
+        const items = frag
+            .replace(/([\.#])/g, " $1")
+            .split(" ")
+            .filter((s) => !!s);
+        if (items.length === 1) {
+            return false;
+        }
+        let allMatch = true;
+        for (const f of items) {
+            if (!matchFragment(f, el)) {
+                allMatch = false;
+                break;
+            }
+        }
+        return allMatch;
+    };
+    if (matchFragment(fragment[0], element)) {
+        let el = element.parentElement;
+        let idx = 1;
+        while (el &&
+            el.parentElement !== el &&
+            idx < fragment.length &&
+            matchFragment(fragment[idx], el)) {
+            idx++;
+            el = el.parentElement;
+        }
+        return idx === fragment.length;
+    }
+    return false;
+}
+match("div #id.class", document.getElementById("id"));
--- Week 11/code.ts
+++ Week 11/code.ts
@@ -1,5 +1,5 @@
 function match(selector: string, element: HTMLElement) {
-  const fragment = selector.split(" ");
+  const fragment = selector.split(" ").reverse();
   const matchFragment = (frag: string, el: HTMLElement) => {
     if (!frag || frag === "*") {
       return true;
@@ -22,19 +22,36 @@ function match(selector: string, element: HTMLElement) {
       .split(" ")
       .filter((s) => !!s);
 
+    if (items.length === 1) {
+      return false;
+    }
+
     let allMatch = true;
 
-    items.forEach((f) => {
+    for (const f of items) {
       if (!matchFragment(f, el)) {
         allMatch = false;
+        break;
       }
-    });
+    }
 
     return allMatch;
   };
 
   if (matchFragment(fragment[0], element)) {
-    // 不断向上找
+    let el = element.parentElement;
+    let idx = 1;
+    while (
+      el &&
+      el.parentElement !== el &&
+      idx < fragment.length &&
+      matchFragment(fragment[idx], el)
+    ) {
+      idx++;
+      el = el.parentElement;
+    }
+
+    return idx === fragment.length;
   }
 
   return false;
